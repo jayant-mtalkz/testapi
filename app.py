03a53358e535014f2aee29a63948f13535e1a56c
jayant-mtalkz <jayant.saxena@mtalkz.com>
2022-06-06 21:51:16 +0530

    add unix time stamp

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, jsonify, request
+import time
+import datetime
+date = datetime.datetime.now()
+unix_time = datetime.datetime.timestamp(date)*1000
+print(unix_time)
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def helloworld():
-    return jsonify({"data": "Hello World"}), 200
+    return jsonify({"data": "Hello World", "request_id": unix_time}), 200
 
 
 if __name__ == '__main__':
